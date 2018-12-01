17846b5aeb6e5ecd788db095593fe80c34003a7a
cdbbnny <aidan.yaklin@gmail.com>
2018-12-01 14:29:44 -0600

    Improve Part 2 output

--- day1/part2.sh
+++ day1/part2.sh
@@ -8,16 +8,19 @@ echo "Parsing ${#freqs[@]} values"
 current=0
 has=0
 
+iter=0
 while [ $has -eq 0 ]; do
+  echo "Iteration $iter -- searched ${#found[@]} numbers"
   for delta in "${freqs[@]}"; do
     found[$current]=1
     prev=$current
     current=$(( $current $delta ))
-    echo "Current: $prev, delta: $delta, result: $current"
+    # echo "Current: $prev, delta: $delta, result: $current"
     if [ -n "${found[$current]}" ]; then
       echo "$current found twice!"
       has=1
       break
     fi
   done
+  iter=$((iter+1))
 done
